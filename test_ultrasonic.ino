6d325f1f4acf8bdff4e7e87a661c79b0686151cd
Sam Ansari <elucidation@gmail.com>
2013-08-26 20:56:37 -0400

    simple working pinging with ultrasonic sensor

---
+++ test_ultrasonic.ino
@@ -0,0 +1,78 @@
+// Define the TRIG and ECHO pins used by the ultrasonic sensor, in my case they were pins 4 and 5.
+#define TRIG 4
+#define ECHO 5
+
+void setup() {
+  // Initialize the serial communication
+  Serial.begin(9600);
+
+  // Set up pins
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+  pinMode(13, OUTPUT);
+}
+
+
+// Uses an ultrasonic sensor to get the measured distance in millimeters
+// TRIG and ECHO must be defined, it also blinks the pin 13 LED for pulse duration
+unsigned long getDistance() {
+  unsigned long t, t_start;
+  
+  // Flush sensor LOW for a second
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  
+  // Pulse for 10 us
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+  
+  // Wait until echo signal is up.
+  while (digitalRead(ECHO) == LOW) {}
+  
+  // Measure echo signal uptime in microseconds. Also blink LED pin 13 during
+  t_start = micros();
+  digitalWrite(13, HIGH);
+  while(digitalRead(ECHO) == HIGH) {}
+  digitalWrite(13, LOW);
+  
+  // Duration in microseconds
+  t = micros() - t_start;
+  
+  // Distance in meters = (duration in seconds) * (speed of sound m/s) / 2
+  // Distance in cm = (t * 1e-6) * (340 * 1e2) / 2 = t * 17/1000
+  // Distance in millimeters = (t * 1e-6) * (340 * 1e3) / 2 = t * 17/100
+  
+  // Return distance in mm, sensor is supposedly accurate to 0.3cm = 3mm
+  return t * 17 / 100;
+}
+
+void prettyprint(int distance_mm) {
+  if (distance_mm < 10) {
+    Serial.print(distance_mm);
+    Serial.println("mm");
+  } else if (distance_mm < 1000) {
+    Serial.print(distance_mm/10);
+    Serial.print('.');
+    Serial.print(distance_mm%10);
+    Serial.println("cm");
+  } else {
+    Serial.print(distance_mm/1000);
+    Serial.print('.');
+    Serial.print(distance_mm%1000);
+    Serial.println("m");
+  }
+}
+
+unsigned long prev = 0;
+void loop() {
+  // Take an ultrasonic reading every second. Results in mm
+  unsigned long dist = getDistance();
+  Serial.print(dist);
+  Serial.println(" mm");
+  // constrain distances above 4m (unreliable at that point)
+  dist = constrain(dist, 0, 4000);
+  prettyprint(dist);
+  
+  delay(10);
+}
