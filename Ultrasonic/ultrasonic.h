928bef1bf984e395d54db185ddab673cdab9e04a
Sam Ansari <elucidation@gmail.com>
2013-08-26 21:44:27 -0400

    Moved ultrasonic code to library, 2 sensors work.

---
+++ Ultrasonic/ultrasonic.h
@@ -0,0 +1,21 @@
+/*
+  Ultrasonic.h - Library for getting ultrasonic sensor readings.
+  Created by Sameer Ansari, Aug 26, 2013.
+  Released into public domain.
+*/
+
+#ifndef Ultrasonic_h
+#define Ultrasonic_h
+
+class Ultrasonic
+{
+public:
+  Ultrasonic(int trigger_pin, int echo_pin);
+  unsigned long getReading();
+
+private:
+  int _trigger_pin, _echo_pin;
+  unsigned long getRawDistance();
+};
+
+#endif
--- ultrasonic.ino
+++ ultrasonic.ino
@@ -1,77 +1,23 @@
-// Define the TRIG and ECHO pins used by the ultrasonic sensor, in my case they were pins 4 and 5.
-#define TRIG 4
-#define ECHO 5
-#define SERVO_PIN 3
+#include <ultrasonic.h>
 
-#include <Servo.h>
-Servo sv;
+// Initialize ultrasonic sensors.
+Ultrasonic sensor_left(2,3);
+Ultrasonic sensor_right(5,6);
 
 void setup() {
-  // Initialize the serial communication
+  // Initialize serial communication.
   Serial.begin(9600);
-
-  // Set up pins
-  pinMode(TRIG, OUTPUT);
-  pinMode(ECHO, INPUT);
-  pinMode(13, OUTPUT);
-  
-  // Set up servo
-  sv.attach(SERVO_PIN);
-  sv.write(90);
-  delay(1000);
-  sv.write(45);
-  delay(100);
+  Serial.println("Start");
 }
 
-
-// Uses an ultrasonic sensor to get the measured distance in millimeters
-// TRIG and ECHO must be defined, it also blinks the pin 13 LED for pulse duration
-int getDistance() {
-  unsigned long t, t_start, c;
-  
-  // Flush sensor LOW for a second
-  digitalWrite(TRIG, LOW);
-  delayMicroseconds(2);
-  
-  // Pulse for 10 us
-  digitalWrite(TRIG, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG, LOW);
-  
-  // Wait until echo signal is up.
-  c = 0;
-  while (digitalRead(ECHO) == LOW && c++ < 10000) {}
-  
-  // Measure echo signal uptime in microseconds. Also blink LED pin 13 during
-  c = 0;
-  t_start = micros();
-  digitalWrite(13, HIGH);
-  while(digitalRead(ECHO) == HIGH && c++ < 10000) {}
-  digitalWrite(13, LOW);
-  
-  // Duration in microseconds
-  t = micros() - t_start;
-  
-  // Distance in meters = (duration in seconds) * (speed of sound m/s) / 2
-  // Distance in cm = (t * 1e-6) * (340 * 1e2) / 2 = t * 17/1000
-  // Distance in millimeters = (t * 1e-6) * (340 * 1e3) / 2 = t * 17/100
-  
-  // Return distance in mm, sensor is supposedly accurate to 0.3cm = 3mm
-  return t * 17 / 100;
-}
-
-int pos = 45;
-int dir = 1;
 void loop() {
-  if (pos <= 0) { dir = 1; }
-  else if (pos >= 180) { dir = -1; }
-  pos = pos + 1 * dir;
-  sv.write(pos);
+  unsigned int left = (unsigned int)sensor_left.getReading();
+  // Wait fo 25 ms, which is roughly how long sound at sea-level takes to travel 8m.
+  // 8m being roundtrip time within sensor range, sounds beyond are considered too dispersed since sensors max effective range is 4m.
   delay(25);
-  
-  int dist = getDistance();
-  Serial.print(pos);
+  unsigned int right = (unsigned int)sensor_right.getReading();
+  Serial.print(left);
   Serial.print(" ");
-  Serial.println(dist);
-  //delay(10);
+  Serial.println(right);
+  delay(25);
 }
