e483a2235376ace5d3b62001b705aea130cc78e6
Sam Ansari <elucidation@gmail.com>
2013-08-26 21:00:12 -0400

    Stuck on servo and did actual scan of env
    Pretty interesting results, semi-encouraging

--- ultrasonic.ino
+++ ultrasonic.ino
@@ -1,6 +1,10 @@
 // Define the TRIG and ECHO pins used by the ultrasonic sensor, in my case they were pins 4 and 5.
 #define TRIG 4
 #define ECHO 5
+#define SERVO_PIN 3
+
+#include <Servo.h>
+Servo sv;
 
 void setup() {
   // Initialize the serial communication
@@ -10,13 +14,20 @@ void setup() {
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
   pinMode(13, OUTPUT);
+  
+  // Set up servo
+  sv.attach(SERVO_PIN);
+  sv.write(90);
+  delay(1000);
+  sv.write(45);
+  delay(100);
 }
 
 
 // Uses an ultrasonic sensor to get the measured distance in millimeters
 // TRIG and ECHO must be defined, it also blinks the pin 13 LED for pulse duration
-unsigned long getDistance() {
-  unsigned long t, t_start;
+int getDistance() {
+  unsigned long t, t_start, c;
   
   // Flush sensor LOW for a second
   digitalWrite(TRIG, LOW);
@@ -28,12 +39,14 @@ unsigned long getDistance() {
   digitalWrite(TRIG, LOW);
   
   // Wait until echo signal is up.
-  while (digitalRead(ECHO) == LOW) {}
+  c = 0;
+  while (digitalRead(ECHO) == LOW && c++ < 10000) {}
   
   // Measure echo signal uptime in microseconds. Also blink LED pin 13 during
+  c = 0;
   t_start = micros();
   digitalWrite(13, HIGH);
-  while(digitalRead(ECHO) == HIGH) {}
+  while(digitalRead(ECHO) == HIGH && c++ < 10000) {}
   digitalWrite(13, LOW);
   
   // Duration in microseconds
@@ -47,17 +60,18 @@ unsigned long getDistance() {
   return t * 17 / 100;
 }
 
-unsigned long prev = 0;
+int pos = 45;
+int dir = 1;
 void loop() {
-  // Take an ultrasonic reading every second. Results in mm
-  unsigned long dist = getDistance();
+  if (pos <= 0) { dir = 1; }
+  else if (pos >= 180) { dir = -1; }
+  pos = pos + 1 * dir;
+  sv.write(pos);
+  delay(25);
   
-  // Low-pass filter it
-  dist = constrain(dist, 0, 4000);
-  signed long diff = (signed long)dist - (signed long)prev;
-  dist = (unsigned long)(prev + diff * 3 / 10);
-  prev = dist;
-  
+  int dist = getDistance();
+  Serial.print(pos);
+  Serial.print(" ");
   Serial.println(dist);
-  delay(10);
+  //delay(10);
 }
